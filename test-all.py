f2772cf33562033d14b14c55df27f9e355b338d3
Andreas Lindner <andili@kth.se>
2019-10-11 22:45:52 +0200

    cosmetic change

--- test-all.py
+++ test-all.py
@@ -1,7 +1,7 @@
 #/bin/bash
 
-(cd exercise1 && make && ./test.py)
-(cd exercise2 && make && ./test.py)
-(cd exercise3 && make && ./test.py)
-(cd exercise4 && make && ./test.py)
+(cd exercise1 && make && ./test.py) > /dev/null
+(cd exercise2 && make && ./test.py) > /dev/null
+(cd exercise3 && make && ./test.py) > /dev/null
+(cd exercise4 && make && ./test.py) > /dev/null
 
