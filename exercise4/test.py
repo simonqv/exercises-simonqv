4f723432c6144ec9ea98c127b2df014484964ac3
Andreas Lindner <andili@kth.se>
2019-10-11 21:10:22 +0200

    Fix2

--- exercise4/test.py
+++ exercise4/test.py
@@ -5,15 +5,15 @@ import subprocess
 
 class test(unittest.TestCase):
 	def test(self):
-	try:
-		subprocess.check_output("unbuffer make attack > res.txt", shell=True)
-	except:
-		pass
-	with open("res.txt", rb) as f:
-		res = f.read()
-	print(res.decode(errors="replace"))
-	self.assertTrue(res.find("root:".encode()) >= 0)
-	self.assertTrue(res.find("student:".encode()) >= 0)
+		try:
+			subprocess.check_output("unbuffer make attack > res.txt", shell=True)
+		except:
+			pass
+		with open("res.txt", rb) as f:
+			res = f.read()
+		print(res.decode(errors="replace"))
+		self.assertTrue(res.find("root:".encode()) >= 0)
+		self.assertTrue(res.find("student:".encode()) >= 0)
 
 
 if __name__ == '__main__':
