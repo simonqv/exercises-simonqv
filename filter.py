3cf368558cd127e773c0de5c74cdd3a11d0ebd22
Simon Larspers Qvist <simonqv@kth.se>
2022-10-18 12:53:45 +0200

    #grading

--- filter.py
+++ filter.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
 
 def filter_text(text):
-    # Your solution.
-    return True
+        keys = []
+        values = []
+        for l in text.split("\n"):
+                if l.startswith("#"):
+                        (key, val) = map(str.strip, l[1:].split("="))
+                        keys.append(key)
+                        values.append(val)
+        for k in keys:
+                for v in values:
+                        if k in v:
+                                return False
+        return True
